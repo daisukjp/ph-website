c907bbcef10750d7f78955ca49c43ef4916082a7
佐藤大亮 <d.sato@diamondhead.tech>
2022-08-10 21:38:57 -0600

    completed page

--- js/main.js
+++ js/main.js
@@ -24,7 +24,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     const so2 = new ScrollObserber('.cover-slide', _inviewAnimation);
+
+    const header = document.querySelector('.header');
+    const _navAnimation = function(el, inview) {
+        if(inview) {
+            header.classList.remove('triggered');
+        } else {
+            header.classList.add('triggered');
+        }
+    }
+
+    const so3 = new ScrollObserber('.nav-trigger', _navAnimation, {once:false});
     
+    new MobileMenu();
 });
 
 
